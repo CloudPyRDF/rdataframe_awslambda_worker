fc20428c8fd8a5f878b7e3cd704bf31acd8bffbb
Kamilbur <kamil.burkiewicz@gmail.com>
2023-01-10 22:49:54 +0100

    Get monitoring to work

--- lambda.py
+++ lambda.py
@@ -4,115 +4,44 @@ import boto3
 import cloudpickle as pickle
 import json
 import os
+import logging
 import time
-from multiprocessing import Process, Pipe
 
-from Inspector import Inspector
+from monitor import CPUAndNetMonitor, EmptyMonitor, monitoring_thread
 
+
+logging.basicConfig(level=logging.DEBUG)
 bucket = os.getenv('bucket')
 debug_command = os.getenv('debug_command', '')
 return_after_debug = os.getenv('return_after_debug', 'False')
 
 krb5ccname = os.getenv('KRB5CCNAME', '/tmp/certs')
-monitor = (os.getenv('monitor', 'False') == 'True')
-results_fname = os.getenv('results_fname', 'results.txt')
+monitoring_on = (os.getenv('monitor', 'False') == 'True')
 
 
-def the_monitor(pipe,rangeid):
-    rangeid = hash(rangeid)
-    def network_measurement():
-        memInfo = ""
-        with open('/proc/net/dev', 'r') as file:
-            memInfo = file.read()
-        my_dict = {}
-        for line in memInfo.split('\n')[2:-1]:
-            splitted = line.split(':')
-            my_dict[splitted[0].strip()] = int(
-                splitted[1].lstrip().split(maxsplit=1)[0]
-            )
-        return my_dict
+def lambda_handler(event, context):
 
-    def inspect_me(rangeid):
-        inspector = Inspector()
-        inspector.inspectAll()
-        inspector.addAttribute("network_rx_bytes", network_measurement())
-        inspector.addAttribute("taskID", rangeid)
+    debug_info = handle_debug(event.get('debug_command'))
+    if debug_info is not None:
+        return debug_info
+    
+    logging.info(f'event {event}')
 
-        return inspector.finish()
+    rdf_range = pickle.loads(base64.b64decode(event['range']))
+    mapper    = pickle.loads(base64.b64decode(event['script']))
+    headers   = pickle.loads(base64.b64decode(event['headers']))
+    cert_file = base64.b64decode(event['cert'])
+    s3_access_key = pickle.loads(base64.b64decode(event['S3_ACCESS_KEY']))
+    s3_secret_key = pickle.loads(base64.b64decode(event['S3_SECRET_KEY']))
+    os.environ['S3_ACCESS_KEY'] = s3_access_key
+    os.environ['S3_SECRET_KEY'] = s3_secret_key
 
-    with open("/tmp/readings.txt","w+") as f:    
-        f.write(" ")
+    logging.info(rdf_range)
+   
+    write_cert(cert_file)
+    declare_headers(headers)
 
-    while True:
-        os.nice(0)
-        with open("/tmp/readings.txt","a") as f:
-            f.write(json.dumps(inspect_me(rangeid)))
-            f.write("\n")
-        # pipe.send(inspect_me())
-        time.sleep(1)
-
-class run_monitoring_thread:
-    """ Context manager for running monitoring
-    """
-    def __init__(self, rdf_range):
-        self.rdf_range = rdf_range
-        self.thread = None
-
-    def __enter__(self):
-        if not monitor:
-            return
-
-        try:
-            os.remove("/tmp/readings.txt")
-        except Exception:
-            pass
-
-        pipe_in, pipe_out = Pipe()
-        self.thread = Process(target=the_monitor, args=(pipe_in, self.rdf_range))
-        self.thread.start()
-        print('monitoring started!')
-
-    def __exit__(self, exception_type, exception_value, traceback):
-        if not monitor:
-            return
-        print('monitoring stopping!')
-        self.thread.terminate()
-        self.thread.join()
-        print('monitoring finished!')
-
-def get_monitoring_results():
-    measurement_results = []
-    if not monitor:
-        return measurement_results
-    with open("/tmp/readings.txt", "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            measurement_results.append(json.loads(line))
-
-def run_mapper(mapper, rdf_range) -> dict:
-    s3_client = boto3.client('s3')
-
-    with run_monitoring_thread(rdf_range):
-        try:
-            hist = mapper(rdf_range)
-        except Exception as exception:
-            return {
-                'statusCode': 500,
-                'errorType': json.dumps(type(exception).__name__),
-                'errorMessage': json.dumps(str(exception)),
-            }
-
-        with open('/tmp/out.pickle', 'wb') as handle:
-            pickle.dump(hist, handle)
-
-        filename = f'partial_{rdf_range.id}_{str(int(time.time() * 1000.0))}.pickle'
-        s3_client.upload_file(f'/tmp/out.pickle', bucket, filename)
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(get_monitoring_results()),
-        'filename': json.dumps(filename)
-    }
+    return run(mapper, rdf_range)
 
 def handle_debug(debug_command: str | None) -> dict | None:
     if debug_command is None:
@@ -124,48 +53,62 @@ def handle_debug(debug_command: str | None) -> dict | None:
             'command': debug_command,
             'command_output': os.popen(f'{debug_command}').read()
         }
-    else:
-        print(debug_command)
-        return None
+
+    logging.info(debug_command)
+    return None
 
 def write_cert(cert_file: bytes):
     with open(f'{krb5ccname}', "wb") as handle:
         handle.write(cert_file)
-    
+
 def declare_headers(headers: list):
     for header_name, header_content in headers:
         header_path = '/tmp/' + header_name
         with open(header_path, 'w') as f:
             f.write(header_content)
-        print(header_name)
+        logging.info(f'Declaring header: {header_name}')
         try:
             ROOT.gInterpreter.Declare(f'#include "{header_path}"')
         except Exception:
-            print(f'Could not declare header {header_name}')
+            logging.error(f'Could not declare header {header_name}')
 
+def run(mapper, rdf_range) -> dict:
+    monitor = get_monitor(rdf_range)
 
-def lambda_handler(event, context):
+    with monitoring_thread(monitor):
+        try:
+            hist = mapper(rdf_range)
+        except Exception as exception:
+            return {
+                'statusCode': 500,
+                'errorType': json.dumps(type(exception).__name__),
+                'errorMessage': json.dumps(str(exception)),
+            }
 
-    debug_info = handle_debug(event.get('debug_command'))
-    if debug_info is not None:
-        return debug_info
+        filename = serialize_and_upload_to_s3(hist, rdf_range.id)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(monitor.get_monitoring_results()),
+        'filename': json.dumps(filename)
+    }
+
+def get_monitor(rdf_range):
+    if monitoring_on:
+        return CPUAndNetMonitor(rdf_range.id)
+    else:
+        return EmptyMonitor()
+
+def serialize_and_upload_to_s3(hist, rangeid):
+    pickled_hist = pickle.dumps(hist)
+    filename = get_unique_filename(rangeid)
+    upload_result_to_s3(pickled_hist, filename)
+    return filename
+
+def get_unique_filename(range_id):
+    timestamp = int(time.time() * 1000.0)
+    return f'partial_{range_id}_{timestamp}.pickle'
     
-    print('event', event)
-
-    rdf_range = pickle.loads(base64.b64decode(event['range'][2:-1]))
-    mapper    = pickle.loads(base64.b64decode(event['script'][2:-1]))
-    headers   = pickle.loads(base64.b64decode(event['headers'][2:-1]))
-    cert_file = base64.b64decode(event['cert'][2:-1])
-    s3_access_key = pickle.loads(base64.b64decode(event['S3_ACCESS_KEY'][2:-1]))
-    s3_secret_key = pickle.loads(base64.b64decode(event['S3_SECRET_KEY'][2:-1]))
-    os.environ['S3_ACCESS_KEY'] = s3_access_key
-    os.environ['S3_SECRET_KEY'] = s3_secret_key
-
-    print(rdf_range)
-   
-    write_cert(cert_file)
-    declare_headers(headers)
-
-    response = run_mapper(mapper, rdf_range)
-
-    return response
+def upload_result_to_s3(obj: bytes, filename: str):
+    s3_client = boto3.client('s3')
+    s3_client.put_object(Body=obj, Bucket=bucket, Key=filename)
---
+++ monitor.py
@@ -0,0 +1,142 @@
+import time
+import os
+import json
+import logging
+from multiprocessing import Process, Pipe
+
+from Inspector import Inspector
+
+
+class CPUAndNetInspector(Inspector):
+    NETWORK_METRICS_NAMES = [
+        'network_bytes_rx',
+        'network_packets_rx',
+        'network_errs_rx',
+        'network_drop_rx',
+        'network_fifo_errors_rx',
+        'network_frame_rx',
+        'network_compressed_rx',
+        'network_multicast_rx',
+        'network_bytes_tx',
+        'network_packets_tx',
+        'network_errs_tx',
+        'network_drop_tx',
+        'network_fifo_errors_tx',
+        'network_colls_tx',
+        'network_carrier_tx',
+        'network_compressed_tx',
+    ]
+
+    def __init__(self):
+        super().__init__()
+
+    def inspectNetwork(self):
+        netInfo = ""
+
+        with open('/proc/net/dev', 'r') as f:
+            netInfo = f.read().splitlines()
+        
+        # skip header lines
+        netInfo = netInfo[2:]
+
+        network_metrics = {name: dict() for name in self.NETWORK_METRICS_NAMES}
+        for line in netInfo:
+            interface, metrics = tuple(line.split(':', maxsplit=1))
+            interface = interface.strip()
+            metrics = metrics.split()
+            for name, value in zip(self.NETWORK_METRICS_NAMES, metrics):
+                network_metrics[name][interface] = int(value)
+
+        self._Inspector__attributes = dict(**self._Inspector__attributes,
+                                           **network_metrics)
+
+    def inspectAll(self):
+        self.inspectNetwork()
+        super().inspectAll()
+
+
+class MonitorBase:
+    OUTPUT_FILEPATH = "/tmp/readings.txt"
+
+    def monitor(self):
+        pass
+
+    def started_info(self):
+        pass
+
+    def finished_info(self):
+        pass
+
+    @staticmethod
+    def create_result_file():
+        with open(MonitorBase.OUTPUT_FILEPATH, "w"):
+            pass
+
+    @staticmethod
+    def get_monitoring_results():
+        lines = []
+        try:
+            with open(MonitorBase.OUTPUT_FILEPATH, "r") as f:
+                lines = f.readlines()
+        except Exception:
+            logging.error('MONITORING ERROR:\n'
+                          'error while getting monitoring results from file')
+
+        return list(map(lambda x: json.loads(x), lines))
+
+
+class CPUAndNetMonitor(MonitorBase):
+    def __init__(self, rangeid,):
+        self.rangehash = hash(rangeid)
+
+    def inspect(self):
+        inspector = CPUAndNetInspector()
+        inspector.inspectAll()
+        inspector.addAttribute("taskID", self.rangehash)
+
+        return inspector.finish()
+
+    def monitor(self):
+        self.create_result_file()
+
+        while True:
+            os.nice(0)
+            inspect_result = self.inspect()
+            with open("/tmp/readings.txt","a") as f:
+                f.write(json.dumps(inspect_result))
+                f.write("\n")
+            time.sleep(1)
+    
+    def started_info(self):
+        logging.info('monitoring started!')
+
+    def finished_info(self):
+        logging.info('monitoring finished!')
+
+
+class EmptyMonitor(MonitorBase):
+    def __init__(self):
+        super().__init__()
+        self.create_result_file()
+
+    def monitor(self):
+        pass
+
+
+class monitoring_thread:
+    """ Context manager for running monitoring
+    """
+    def __init__(self, monitor):
+        self.monitor = monitor
+        self.thread = None
+
+    def __enter__(self):
+        self.monitor.started_info()
+        self.thread = Process(target=self.monitor.monitor)
+        self.thread.start()
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.thread.terminate()
+        self.thread.join()
+        self.monitor.finished_info()
