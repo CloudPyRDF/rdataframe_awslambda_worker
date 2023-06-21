0690904b9869496fa4eca9f716b439ff6216d360
Kamilbur <kamil.burkiewicz@gmail.com>
2023-06-22 01:32:20 +0200

    Add configuration for async-reduce

--- lambda.py
+++ lambda.py
@@ -17,6 +17,7 @@ return_after_debug = os.getenv('return_after_debug', 'False')
 
 krb5ccname = os.getenv('KRB5CCNAME', '/tmp/certs')
 monitoring_on = (os.getenv('monitor', 'False') == 'True')
+suffix = os.getenv('suffix', 'out')
 
 
 def lambda_handler(event, context):
@@ -32,6 +33,7 @@ def lambda_handler(event, context):
     headers   = pickle.loads(base64.b64decode(event['headers']))
     prefix    = pickle.loads(base64.b64decode(event['prefix']))
     cert_file = base64.b64decode(event['cert'])
+    file_count = pickle.loads(base64.b64decode(event['file_count']))
     s3_access_key = pickle.loads(base64.b64decode(event['S3_ACCESS_KEY']))
     s3_secret_key = pickle.loads(base64.b64decode(event['S3_SECRET_KEY']))
     os.environ['S3_ACCESS_KEY'] = s3_access_key
@@ -42,7 +44,7 @@ def lambda_handler(event, context):
     write_cert(cert_file)
     declare_headers(headers)
 
-    return run(mapper, rdf_range, prefix)
+    return run(mapper, rdf_range, prefix, file_count)
 
 def handle_debug(debug_command: str | None) -> dict | None:
     if debug_command is None:
@@ -73,7 +75,7 @@ def declare_headers(headers: list):
         except Exception:
             logging.error(f'Could not declare header {header_name}')
 
-def run(mapper, rdf_range, prefix) -> dict:
+def run(mapper, rdf_range, prefix, file_count) -> dict:
     monitor = get_monitor(rdf_range)
 
     with monitoring_thread(monitor):
@@ -86,10 +88,12 @@ def run(mapper, rdf_range, prefix) -> dict:
                 'errorMessage': json.dumps(str(exception)),
             }
 
-        filename = serialize_and_upload_to_s3(hist, prefix, rdf_range.id)
+        N = reduce_tree_leafs_count(file_count)
+        filename = serialize_and_upload_to_s3(hist, prefix, N + rdf_range.id, file_count, suffix=suffix)
 
-        monitoring_results = monitor.get_monitoring_results()
-        serialize_and_upload_to_s3(monitoring_results, f'monitor-{prefix}', rdf_range.id)
+        if monitoring_on:
+            monitoring_results = monitor.get_monitoring_results()
+            serialize_and_upload_to_s3(monitoring_results, f'monitor-{prefix}', rdf_range.id, file_count)
     return {
         'statusCode': 200,
         'filename': json.dumps(filename)
@@ -101,14 +105,17 @@ def get_monitor(rdf_range):
     else:
         return EmptyMonitor()
 
-def serialize_and_upload_to_s3(hist, prefix, rangeid):
+def reduce_tree_leafs_count(file_count):
+    return 2 ** (file_count - 1).bit_length()
+
+def serialize_and_upload_to_s3(hist, prefix, rangeid, file_count, suffix=''):
     pickled_hist = pickle.dumps(hist)
-    filename = get_unique_filename(prefix, rangeid)
+    filename = get_unique_filename(prefix, rangeid, file_count, suffix)
     upload_result_to_s3(pickled_hist, filename)
     return filename
 
-def get_unique_filename(prefix, range_id):
-    return f'output/{prefix}/partial_{range_id}'
+def get_unique_filename(prefix, range_id, file_count, suffix):
+    return f'output/{prefix}/partial_{range_id}_{file_count}.{suffix}'
     
 def upload_result_to_s3(obj: bytes, filename: str):
     s3_client = boto3.client('s3')
